be979a16c00f56b5b99ee509844a2842f8006100
Mohammad Kermani <mkermani144@gmail.com>
2026-02-15 14:44:28 +0330

    Update my information

--- src/app.rs
+++ src/app.rs
@@ -58,9 +58,53 @@ pub fn App() -> impl IntoView {
 #[component]
 fn HomePage() -> impl IntoView {
     view! {
-        <section>
+        <section class="text-center">
             <h1 class="text-3xl font-semibold tracking-tight">"Mohammad Kermani"</h1>
-            <p class="mt-3 text-neutral-300">"I like to build things that people use."</p>
+            <p class="mt-4 text-neutral-300">
+                "Software Engineer"
+            </p>
+            <p class="mt-2 text-neutral-400">
+                "I love building things that people use."
+            </p>
+
+            <ul class="mt-8 flex flex-wrap items-center justify-center gap-5 text-neutral-200">
+                <li>
+                    <a
+                        class="underline decoration-neutral-500 underline-offset-4 hover:text-white"
+                        href="https://github.com/mkermani144"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        "GitHub"
+                    </a>
+                </li>
+                <li>
+                    <a
+                        class="underline decoration-neutral-500 underline-offset-4 hover:text-white"
+                        href="https://linkedin.com/in/mkermani144"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        "LinkedIn"
+                    </a>
+                </li>
+                <li>
+                    <a
+                        class="underline decoration-neutral-500 underline-offset-4 hover:text-white"
+                        href="https://x.com/mkermani144"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        "X"
+                    </a>
+                </li>
+            </ul>
+
+            <p class="mt-8">
+                <a class="text-neutral-100 underline underline-offset-4 hover:text-white" href="/blog.html">
+                    "Read the blog"
+                </a>
+            </p>
         </section>
     }
 }
